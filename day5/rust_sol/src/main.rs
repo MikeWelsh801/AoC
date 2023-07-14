5ab8d9bf3dd64451d2ebf9424f23036dd3019418
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-07-14 09:46:36 -0600

    solve part 2

--- day5/rust_sol/src/main.rs
+++ day5/rust_sol/src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Couldn't read file.");
 
+    // part 1
+
     let mut stacks: Vec<Vec<char>> = vec![
         Vec::new(),
         Vec::new(),
@@ -37,6 +39,57 @@ fn main() {
         print!("{}", stacks[i].pop().unwrap());
     });
     println!("");
+
+    // part 2
+
+    let mut stacks: Vec<Vec<char>> = vec![
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+        Vec::new(),
+    ];
+
+    let mut filling_stacks = true;
+
+    for line in contents.lines() {
+        if line.is_empty() {
+            stacks.iter_mut().for_each(|stack| {
+                stack.reverse();
+            });
+            filling_stacks = false;
+            continue;
+        }
+
+        match filling_stacks {
+            true => fill_stacks(&mut stacks, line),
+            false => swap_stacks_9001(&mut stacks, line),
+        }
+    }
+
+    print!("Answer 2: ");
+    (0..9).for_each(|i| {
+        print!("{}", stacks[i].pop().unwrap());
+    });
+    println!("");
+}
+
+fn swap_stacks_9001(stacks: &mut Vec<Vec<char>>, line: &str) {
+    let (move_num, from_index, to_index) = extract_nums(line);
+    let mut vals = Vec::new();
+
+    (0..move_num).for_each(|_| {
+        let val = stacks[from_index].pop().unwrap();
+        vals.push(val);
+    });
+
+    vals.into_iter().rev().for_each(|val| {
+        stacks[to_index].push(val);
+    })
 }
 
 fn swap_stacks(stacks: &mut Vec<Vec<char>>, line: &str) {
