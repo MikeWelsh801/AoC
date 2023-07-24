4fa63088dbc9976f07ea035fb27b72b769cf972f
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-07-24 13:28:06 -0600

    Add print_path function and refactor.

--- day12/rust_sol/src/main.rs
+++ day12/rust_sol/src/main.rs
@@ -4,32 +4,67 @@ fn main() {
     let contents = fs::read_to_string("input.txt").expect("Couldn't read file.");
 
     let (grid, s_pos, e_pos) = build_grid(&contents);
-    let path_len = bfs(&grid, s_pos, e_pos);
-    let shortest_len = get_shortest_a_len(&grid, e_pos);
+    let (path_len, prev) = bfs(&grid, s_pos, e_pos);
+    let (shortest_len, shortest_prev) = get_shortest_a_len(&grid, e_pos);
 
+    print_path(&grid, &prev, e_pos);
     println!("Answer 1: {path_len}");
+
+    print_path(&grid, &shortest_prev, e_pos);
     println!("Answer 2: {shortest_len}");
 }
 
-fn get_shortest_a_len(grid: &Vec<Vec<char>>, e_pos: (usize, usize)) -> u32 {
-    let mut min = u32::MAX;
-    grid.iter().enumerate().for_each(|(i, line)| {
-        line.iter().enumerate().for_each(|(j, ele)| {
-            if *ele == 'a' {
-                let shortest = bfs(&grid, (i, j), e_pos);
-                if shortest < min {
-                    min = shortest;
-                }
+fn print_path(
+    grid: &Vec<Vec<char>>,
+    prev: &Vec<Vec<Option<(usize, usize)>>>,
+    e_pos: (usize, usize),
+) {
+    let mut path = vec![];
+    let mut pos = e_pos;
+    path.push(e_pos);
+
+    while let Some(node) = prev[pos.0][pos.1] {
+        path.push(node);
+        pos = node;
+    }
+
+    (0..grid.len()).into_iter().for_each(|i| {
+        (0..grid[i].len()).into_iter().for_each(|j| {
+            if path.contains(&(i, j)) {
+                print!("#");
+            } else {
+                print!(".");
             }
-        })
+        });
+        println!();
     });
-    min
+
 }
 
-fn bfs(grid: &Vec<Vec<char>>, s_pos: (usize, usize), e_pos: (usize, usize)) -> u32 {
+fn get_shortest_a_len(grid: &Vec<Vec<char>>, e_pos: (usize, usize)) -> (u32, Vec<Vec<Option<(usize, usize)>>>) {
+    grid.iter()
+        .enumerate()
+        .map(|(i, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, ele)| **ele == 'a')
+                .map(|(j, _)| bfs(&grid, (i, j), e_pos))
+                .min()
+                .unwrap()
+        })
+        .min()
+        .unwrap()
+}
+
+fn bfs(
+    grid: &Vec<Vec<char>>,
+    s_pos: (usize, usize),
+    e_pos: (usize, usize),
+) -> (u32, Vec<Vec<Option<(usize, usize)>>>) {
     let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
     let mut queue = vec![];
-    let mut dist = vec![vec![i32::MAX; grid[0].len()]; grid.len()];
+    let mut dist = vec![vec![u32::MAX; grid[0].len()]; grid.len()];
+    let mut prev: Vec<Vec<Option<(usize, usize)>>> = vec![vec![None; grid[0].len()]; grid.len()];
     let mut done = false;
 
     // distance to start is zero, and start has been visited.
@@ -46,17 +81,18 @@ fn bfs(grid: &Vec<Vec<char>>, s_pos: (usize, usize), e_pos: (usize, usize)) -> u
             if !visited[node.0][node.1] {
                 visited[node.0][node.1] = true;
                 dist[node.0][node.1] = dist[curr.0][curr.1] + 1;
+                prev[node.0][node.1] = Some(curr);
                 queue.push(*node);
 
                 // stop when we reach the end.
-                if grid[node.0][node.1] == 'E' {
+                if *node == e_pos {
                     done = true;
                     return;
                 }
             }
         });
     }
-    dist[e_pos.0][e_pos.1] as u32
+    (dist[e_pos.0][e_pos.1], prev)
 }
 
 fn get_adj_nodes(curr: (usize, usize), grid: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
