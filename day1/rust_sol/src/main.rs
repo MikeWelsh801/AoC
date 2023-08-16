3c2a8a8231574878501ba9fc6e1e487b177380db
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-08-16 14:22:10 -0600

    changed sorting algorithm used in day1 rust solution to qsort variation

--- day1/rust_sol/src/main.rs
+++ day1/rust_sol/src/main.rs
@@ -5,16 +5,16 @@ fn main() {
 
     let mut total_calories = 0;
     let mut list: Vec<i32> = vec![];
-    for line in contents.split('\n') {
+    contents.lines().for_each(|line| {
         if let Ok(value) = line.trim().parse::<i32>() {
             total_calories += value;
         } else {
             list.push(total_calories);
             total_calories = 0;
         }
-    }
+    });
 
-    list.sort_by(|a, b| b.cmp(a));
+    list.sort_unstable_by(|a, b| b.cmp(a));
     println!("Answer 1: {}", list[0]);
     println!("Answer 2: {}", list[0] + list[1] + list[2]);
 }
